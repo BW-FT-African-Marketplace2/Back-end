51a3b2c5e46462e5fd91209d3a91d12b3376a834
oscar-lopez-projects <d1lopeztv@gmail.com>
2020-09-20 16:35:26 -0700

    inital

---
+++ src/main/java/backend/lambdaschool/AfricanMarketplaceApplication.java
@@ -0,0 +1,14 @@
+package backend.lambdaschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfricanMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfricanMarketplaceApplication.class, args);
+        System.out.println("hello");
+    }
+
+}
---
+++ src/test/java/backend/lambdaschool/AfricanMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.lambdaschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfricanMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
